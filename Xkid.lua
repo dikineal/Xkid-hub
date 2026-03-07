2f3bc0301ded3d0f9cf6d08af87d5e43b557891a
dikineal <130257900+dikineal@users.noreply.github.com>
2026-03-08 01:48:03 +0800

    Update Xkid.lua

--- Xkid.lua
+++ Xkid.lua
@@ -1,260 +1,167 @@
+-- Fluent UI Library - Contoh Dasar
+-- Copy-paste ke Roblox Executor
+
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
+-- ===== MEMBUAT WINDOW =====
 local Window = Fluent:CreateWindow({
-    Title = "Fluent " .. Fluent.Version,
-    SubTitle = "by dawid",
+    Title = "My Fluent Script",
+    SubTitle = "v1.0 - Made with Fluent",
     TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
-    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Size = UDim2.new(0, 580, 0, 460),
+    Acrylic = true, -- Blur effect
     Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+    MinimizeKey = Enum.KeyCode.LeftControl
 })
 
---Fluent provides Lucide Icons https://lucide.dev/icons/ for the tabs, icons are optional
-local Tabs = {
-    Main = Window:AddTab({ Title = "Main", Icon = "" }),
-    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
-}
+-- ===== TAB 1: HOME =====
+local Tabs = {}
+Tabs.Home = Window:AddTab({ Icon = "rbxasset://textures/Cursor.png", Title = "Home" })
 
-local Options = Fluent.Options
+Tabs.Home:AddParagraph({
+    Title = "Welcome!",
+    Content = "Ini adalah contoh script menggunakan Fluent UI Library."
+})
 
-do
-    Fluent:Notify({
-        Title = "Notification",
-        Content = "This is a notification",
-        SubContent = "SubContent", -- Optional
-        Duration = 5 -- Set to nil to make the notification not disappear
-    })
+Tabs.Home:AddButton({
+    Title = "Click Me",
+    Description = "Tombol yang bisa di-click",
+    Callback = function()
+        print("Button clicked!")
+    end
+})
 
+Tabs.Home:AddToggle("Toggle1", {
+    Title = "Enable Feature",
+    Default = false,
+    Callback = function(Value)
+        print("Toggle state:", Value)
+    end
+})
 
+-- ===== TAB 2: FEATURES =====
+Tabs.Features = Window:AddTab({ Icon = "rbxasset://textures/Cursor.png", Title = "Features" })
 
-    Tabs.Main:AddParagraph({
-        Title = "Paragraph",
-        Content = "This is a paragraph.\nSecond line!"
-    })
+-- Slider
+Tabs.Features:AddSlider("Slider1", {
+    Title = "Speed",
+    Description = "Ubah kecepatan",
+    Min = 0,
+    Max = 100,
+    Rounding = 1,
+    Default = 50,
+    Callback = function(Value)
+        print("Speed:", Value)
+    end
+})
 
+-- Input Box
+Tabs.Features:AddInput("Input1", {
+    Title = "Text Input",
+    Default = "",
+    Placeholder = "Enter text here...",
+    Numeric = false,
+    Finished = false,
+    Callback = function(Value)
+        print("Input:", Value)
+    end
+})
 
+-- Dropdown
+Tabs.Features:AddDropdown("Dropdown1", {
+    Title = "Choose Option",
+    Values = {"Option 1", "Option 2", "Option 3", "Option 4"},
+    Multi = false,
+    Default = 1,
+    Callback = function(Value)
+        print("Selected:", Value)
+    end
+})
 
-    Tabs.Main:AddButton({
-        Title = "Button",
-        Description = "Very important button",
-        Callback = function()
-            Window:Dialog({
-                Title = "Title",
-                Content = "This is a dialog",
-                Buttons = {
-                    {
-                        Title = "Confirm",
-                        Callback = function()
-                            print("Confirmed the dialog.")
-                        end
-                    },
-                    {
-                        Title = "Cancel",
-                        Callback = function()
-                            print("Cancelled the dialog.")
-                        end
-                    }
-                }
-            })
-        end
-    })
+-- Multi Select Dropdown
+Tabs.Features:AddMultiselect("Multiselect1", {
+    Title = "Multi Select",
+    Values = {"Item 1", "Item 2", "Item 3"},
+    Default = {},
+    Callback = function(Value)
+        print("Selected items:", Value)
+    end
+})
 
+-- ===== TAB 3: SETTINGS =====
+Tabs.Settings = Window:AddTab({ Icon = "rbxasset://textures/Cursor.png", Title = "Settings" })
 
+-- Color Picker
+Tabs.Settings:AddColorpicker("Colorpicker1", {
+    Title = "Pick Color",
+    Default = Color3.fromRGB(255, 0, 0),
+    Callback = function(Value)
+        print("Color:", Value)
+    end
+})
 
-    local Toggle = Tabs.Main:AddToggle("MyToggle", {Title = "Toggle", Default = false })
+-- Keybind
+Tabs.Settings:AddKeybind("Keybind1", {
+    Title = "Toggle Keybind",
+    Default = Enum.KeyCode.T,
+    Hold = false,
+    Callback = function()
+        print("Keybind activated!")
+    end
+})
 
-    Toggle:OnChanged(function()
-        print("Toggle changed:", Options.MyToggle.Value)
-    end)
+-- Section
+local Section = Tabs.Settings:AddSection("Other Settings")
 
-    Options.MyToggle:SetValue(false)
+Tabs.Settings:AddToggle("Toggle2", {
+    Title = "Auto Save",
+    Default = true,
+    Callback = function(Value)
+        print("Auto save:", Value)
+    end
+})
 
+Tabs.Settings:AddButton({
+    Title = "Reset Settings",
+    Description = "Reset semua setting ke default",
+    Callback = function()
+        print("Settings reset!")
+    end
+})
 
-    
-    local Slider = Tabs.Main:AddSlider("Slider", {
-        Title = "Slider",
-        Description = "This is a slider",
-        Default = 2,
-        Min = 0,
-        Max = 5,
-        Rounding = 1,
-        Callback = function(Value)
-            print("Slider was changed:", Value)
-        end
-    })
+-- ===== TAB 4: INFO =====
+Tabs.Info = Window:AddTab({ Icon = "rbxasset://textures/Cursor.png", Title = "Info" })
 
-    Slider:OnChanged(function(Value)
-        print("Slider changed:", Value)
-    end)
+Tabs.Info:AddParagraph({
+    Title = "About This Script",
+    Content = "Script ini dibuat menggunakan Fluent UI Library. Fluent adalah library UI modern dengan desain yang bagus dan banyak fitur."
+})
 
-    Slider:SetValue(3)
+Tabs.Info:AddButton({
+    Title = "Destroy UI",
+    Description = "Tutup script ini",
+    Callback = function()
+        Window:Destroy()
+        print("UI destroyed!")
+    end
+})
 
-
-
-    local Dropdown = Tabs.Main:AddDropdown("Dropdown", {
-        Title = "Dropdown",
-        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
-        Multi = false,
-        Default = 1,
-    })
-
-    Dropdown:SetValue("four")
-
-    Dropdown:OnChanged(function(Value)
-        print("Dropdown changed:", Value)
-    end)
-
-
-    
-    local MultiDropdown = Tabs.Main:AddDropdown("MultiDropdown", {
-        Title = "Dropdown",
-        Description = "You can select multiple values.",
-        Values = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen"},
-        Multi = true,
-        Default = {"seven", "twelve"},
-    })
-
-    MultiDropdown:SetValue({
-        three = true,
-        five = true,
-        seven = false
-    })
-
-    MultiDropdown:OnChanged(function(Value)
-        local Values = {}
-        for Value, State in next, Value do
-            table.insert(Values, Value)
-        end
-        print("Mutlidropdown changed:", table.concat(Values, ", "))
-    end)
-
-
-
-    local Colorpicker = Tabs.Main:AddColorpicker("Colorpicker", {
-        Title = "Colorpicker",
-        Default = Color3.fromRGB(96, 205, 255)
-    })
-
-    Colorpicker:OnChanged(function()
-        print("Colorpicker changed:", Colorpicker.Value)
-    end)
-    
-    Colorpicker:SetValueRGB(Color3.fromRGB(0, 255, 140))
-
-
-
-    local TColorpicker = Tabs.Main:AddColorpicker("TransparencyColorpicker", {
-        Title = "Colorpicker",
-        Description = "but you can change the transparency.",
-        Transparency = 0,
-        Default = Color3.fromRGB(96, 205, 255)
-    })
-
-    TColorpicker:OnChanged(function()
-        print(
-            "TColorpicker changed:", TColorpicker.Value,
-            "Transparency:", TColorpicker.Transparency
-        )
-    end)
-
-
-
-    local Keybind = Tabs.Main:AddKeybind("Keybind", {
-        Title = "KeyBind",
-        Mode = "Toggle", -- Always, Toggle, Hold
-        Default = "LeftControl", -- String as the name of the keybind (MB1, MB2 for mouse buttons)
-
-        -- Occurs when the keybind is clicked, Value is `true`/`false`
-        Callback = function(Value)
-            print("Keybind clicked!", Value)
-        end,
-
-        -- Occurs when the keybind itself is changed, `New` is a KeyCode Enum OR a UserInputType Enum
-        ChangedCallback = function(New)
-            print("Keybind changed!", New)
-        end
-    })
-
-    -- OnClick is only fired when you press the keybind and the mode is Toggle
-    -- Otherwise, you will have to use Keybind:GetState()
-    Keybind:OnClick(function()
-        print("Keybind clicked:", Keybind:GetState())
-    end)
-
-    Keybind:OnChanged(function()
-        print("Keybind changed:", Keybind.Value)
-    end)
-
-    task.spawn(function()
-        while true do
-            wait(1)
-
-            -- example for checking if a keybind is being pressed
-            local state = Keybind:GetState()
-            if state then
-                print("Keybind is being held down")
-            end
-
-            if Fluent.Unloaded then break end
-        end
-    end)
-
-    Keybind:SetValue("MB2", "Toggle") -- Sets keybind to MB2, mode to Hold
-
-
-    local Input = Tabs.Main:AddInput("Input", {
-        Title = "Input",
-        Default = "Default",
-        Placeholder = "Placeholder",
-        Numeric = false, -- Only allows numbers
-        Finished = false, -- Only calls callback when you press enter
-        Callback = function(Value)
-            print("Input changed:", Value)
-        end
-    })
-
-    Input:OnChanged(function()
-        print("Input updated:", Input.Value)
-    end)
-end
-
-
--- Addons:
--- SaveManager (Allows you to have a configuration system)
--- InterfaceManager (Allows you to have a interface managment system)
-
--- Hand the library over to our managers
+-- ===== KONFIGURASI PENYIMPANAN =====
 SaveManager:SetLibrary(Fluent)
 InterfaceManager:SetLibrary(Fluent)
 
--- Ignore keys that are used by ThemeManager.
--- (we dont want configs to save themes, do we?)
+-- Menambahkan ignore list untuk SaveManager
 SaveManager:IgnoreThemeSettings()
-
--- You can add indexes of elements the save manager should ignore
 SaveManager:SetIgnoreIndexes({})
 
--- use case for doing it this way:
--- a script hub could have themes in a global folder
--- and game configs in a separate folder per game
-InterfaceManager:SetFolder("FluentScriptHub")
-SaveManager:SetFolder("FluentScriptHub/specific-game")
-
-InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+-- Membuat folder config
 SaveManager:BuildConfigSection(Tabs.Settings)
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
 
-
-Window:SelectTab(1)
-
-Fluent:Notify({
-    Title = "Fluent",
-    Content = "The script has been loaded.",
-    Duration = 8
-})
-
--- You can use the SaveManager:LoadAutoloadConfig() to load a config
--- which has been marked to be one that auto loads!
+-- Load config jika ada
 SaveManager:LoadAutoloadConfig()
+
+print("Fluent UI loaded successfully!")
+print("Press LEFT CTRL to minimize/maximize")
