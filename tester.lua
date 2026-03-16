952a9ee54f92c81d53eac1e5ef14483697c25109
dikineal <130257900+dikineal@users.noreply.github.com>
2026-03-17 05:05:18 +0800

    Update tester.lua

--- tester.lua
+++ tester.lua
@@ -1,25 +1,95 @@
-local library = loadstring(game:HttpGet("https://thebasement.ink/BSMTUI"))()
-local window = library:Create("BSMT Hub", UDim2.new(0, 500, 0, 400))
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/fhrdimybds-byte/Redz-ui-lib/refs/heads/main/Lua"))()
 
-local Tab = window:Tab("Main", "rbxassetid://10734950309")
-local Section = Tab:Section("Controls")
+local Window = redzlib:MakeWindow({
+  Title = "redz Hub : Blox Fruits",
+  SubTitle = "by redz9999",
+  SaveFolder = "testando | redz lib v5.lua"
+})
 
-Section:Button("Notify Me", function()
-    library:Notify({ Title = "Yo!", Text = "It works!", Type = "Info" })
+Window:AddMinimizeButton({
+    Button = { Image = "rbxassetid://71014873973869", BackgroundTransparency = 0 },
+    Corner = { CornerRadius = UDim.new(35, 1) },
+})
+
+local Tab1 = Window:MakeTab({"Um", "cherry"})
+
+Tab1:AddDiscordInvite({
+    Name = "Name Hub",
+    Description = "Join server",
+    Logo = "rbxassetid://18751483361",
+    Invite = "Link discord invite",
+})
+
+Window:SelectTab(Tab1)
+
+local Section = Tab1:AddSection({"Section"})
+
+local Paragraph = Tab1:AddParagraph({"Paragraph", "This is a Paragraph\nSecond Line"})
+
+  local Dialog = Window:Dialog({
+    Title = "Dialog",
+    Text = "This is a Dialog",
+    Options = {
+      {"Confirm", function()
+        
+      end},
+      {"Maybe", function()
+        
+      end},
+      {"Cancel", function()
+        
+      end}
+    }
+  })
+  
+  Tab1:AddButton({"Print", function(Value)
+print("Hello World!")
+end})
+
+local Toggle1 = Tab1:AddToggle({
+  Name = "Toggle",
+  Description = "This is a <font color='rgb(88, 101, 242)'>Toggle</font> Example",
+  Default = false 
+})
+Toggle1:Callback(function(Value)
+ 
 end)
 
-Section:Slider("WalkSpeed", 10, 100, 16, function(val)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val
-end)
+Tab1:AddToggle({
+    Name = "Toggle",
+    Default = false,
+    Callback = function(v)
 
-Section:Dropdown("Mode", {"Classic", "Pro", "Insane"}, "Classic", function(mode)
-    print("Selected:", mode)
-end)
+    end
+})
 
-Section:Toggle("WallHack", false, function(state)
-    print("WallHack active?", state)
-end)
+Tab1:AddSlider({
+  Name = "Speed",
+  Min = 1,
+  Max = 100,
+  Increase = 1,
+  Default = 16,
+  Callback = function(Value)
+  
+  end
+})
 
-Section:TextBox("FOV Size", "90", function(text)
-    print("FOV:", text)
-end)
+local Dropdown = Tab1:AddDropdown({
+  Name = "Players List",
+  Description = "Select the <font color='rgb(88, 101, 242)'>Number</font>",
+  Options = {"one", "two", "three"},
+  Default = "two",
+  Flag = "dropdown teste",
+  Callback = function(Value)
+    
+  end
+})
+
+Tab1:AddTextBox({
+  Name = "Name item",
+  Description = "1 Item on 1 Server", 
+  PlaceholderText = "item only",
+  Callback = function(Value)
+    
+  end
+})
